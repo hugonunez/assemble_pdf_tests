4bed0cd0cab23c8c6390e87d00213885a4fb0435
HugoNR <hugonromante@outlook.com>
2020-11-09 15:47:20 -0300

    debugging what breaks the script

--- src/assemblepdf.js
+++ src/assemblepdf.js
@@ -8,7 +8,10 @@
 * The way in which this widgets are assigned is managed by a chain of responsibility
 * that will handle edge cases or default to a solution.
 *
+* Must use vanilla JS without ES6 for compatibility issues.
 * */
+
+
 
 var pageHeight = (window.customSize)? window.customSize : 1056;
 
@@ -34,11 +37,40 @@ function getHeight(element) {
     return height;
 }
 
+var default_page_height = (window.customSize)? window.customSize : 1056;
 
-// Note: Must use vanilla Javascript support whtmltopdf and other pdf converters which do not support es6 and jQuery
+var constants = {
+    ALL_WIDGETS_SELECTOR: "#main > div.mail__container > div",
+    ALL_PAGES_SELECTOR: '#print > div.pdf-page > div',
+    TABLE_WIDGET_SELECTOR: "table.widget-product",
+    ALL_MAIL_CONTAINERS: "#main > div.mail__container",
+    PAGE_HEIGHT: default_page_height,
+    PRINT_SELECTOR: 'print',
+    DEFAULT_SKIP_FOOTER_THRESHOLD: default_page_height*0.1,
+    DEFAULT_SCALE_DOWN: default_page_height*0.05
+}
+
+
 window.onload = function () {
 
+    assemblePDF({
+        items: [], 
+        pages: [],
+        pageHeight: 1000,
+        skipFooterThreshold: 100,
+        scaleDownThreshold: 50,
+        print: null,
+        mode: 'portrait'
+    });
+
+    hideRemainingElements();
+    markDocAsReady();
+
+}
+
+function assemblePDF(props) {
     console.log('---------------------- Stage 1 ------------------------');
+  
 
     var total = 0
     var widgets = document.querySelectorAll("#main > div.mail__container > div");
@@ -59,12 +91,38 @@ window.onload = function () {
         }
 
         total += itemHeight;
+        widgets[i].style.border = '1px dashed red'
         page.appendChild(widgets[i]);
         console.log("Widget added to page ", i);
     }
+}
 
+function makeFooterAndWrapper (props){
+    var footerWrapper = document.createElement('div');
+    var footer = document.createElement("div");
+    var rightSection = document.createElement('small');
+    var footerSignature = document.createElement('small');
+    var customWidth = props.width;
+
+    //footer
+    footer.style['padding'] = '10px'
+    footer.style['font-weight'] = 'bold';
+    footer.style['margin-top'] = '10px';
+    footer.style['margin-bottom'] = '10px';
+    footer.style['display'] = 'flex';
+    footer.style['justify-content'] = 'space-between';
+    footer.style['width'] = customWidth + 'px'
+    footer.setAttribute('class', 'pdf-footer');
+
+    footerSignature.style['color'] = 'grey';
+   
+}
+
+function hideRemainingElements() {
     // Hide extra element
     document.querySelector("#main > div.mail__container").style.display = "none";
+}
+function markDocAsReady() {
     console.log('---------------------- COMPLETE------------------------');
 
     if (window.pdfdone) {
@@ -77,5 +135,4 @@ window.onload = function () {
         page.appendChild(readyElem);
     }
     window.status = 'ready';
-
 }
