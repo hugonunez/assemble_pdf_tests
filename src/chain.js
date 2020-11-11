9b6c047f27031827e5e577ce6702244880bdf928
hugonr <hugonunez@thedogcompany.cl>
2020-11-11 15:18:37 -0300

    REfactor to chain of responsability

--- src/chain.js
+++ src/chain.js
@@ -1,81 +1,127 @@
-//CHAIN OF RESPONSABILITY
-//
-// Let w = {offsetHeight, img}
-//
-// Let W = [w1,w2,w3,...wn] = Widgets
-//
-// Let P = [p1] = Pages
-//
-// Let scaleDownThreshold = x;
-//
-// Let changeLayoutTreshold = y;
-//
-// 0.- Calculate sum W heights
-//
-// 1.- Try to fit W in P[P.length-1]
-//
-// 2.- Iterate over W and fit as many widgets as posible into last page, recurse.
-//
-// 3.-
 
-const setup = function () {
-    const widgets = Getters.getWidgets();
-    const print = Getters.getPrint();
-    const pages = Getters.getPages();
-    const skipPageThreshold =  constants.DEFAULT_SKIP_PAGE_THRESHOLD;
-    //Creates necessary objects for chain of responsibility
-    const init = {widgets, pages, pageHeight, skipPageThreshold, print}
-    return init;
-}
 
-const Handler = function (handle, canDo, name = 'New Handler', init){
-    this.next = null;
-    this.done = false;
-    this.handle = handle;
-    this.attempt = function (init){
-        const valid = canDo(init);
-        if (valid){
-            this.handle(init)
-            this.setDone();
-        }else {
-            if (this.next) {
-                this.next.attempt(...args)
-            }else {
-                this.setDone();
-                throw 'No next function assigned to ' + name
-            }
+
+//Handler Abstract Class
+var Handler = function (){
+    this.next = {
+        handleRequest: function (request){
+            console.log("ALL HANDLERS HAVE BEEN TRIED, NONE MATCHED.")
         }
-    };
-    this.setNextStep = function (handler) {
-        this.next = handler
     }
-    this.setDone = function (){
-        this.done = true;
+};
+//Abstract Class properties
+Handler.prototype.setNext = function (next) {
+    this.next = next;
+    return next;
+};
+Handler.prototype.handleRequest = function (request){};
+
+/*
+* Define Strategies to assign widgets
+* */
+
+/////////////////      Strategy  1    /////////////////////
+var CheckIfPageFinished = function (){}
+CheckIfPageFinished.prototype = new Handler();
+CheckIfPageFinished.prototype.handleRequest = function (request){
+    if(request.isPageFinished){
+        console.log("CheckIfPageFinished")
+        Commander.execute('createNewPage', {
+            print: request.print,
+            pages: request.pages,
+            mode: request.mode
+        });
+        Commander.execute('resetPageStatus')
     }
-    return this
+    this.next.handleRequest(request);
 }
 
-//DEFINE HANDLERS
-const handlers = {
-    fitAllWidgetsIntoPage: Handler(function (init){
-        //FIT ALL WIDGETS INTO PAGE
-        const page = init.pages[init.pages.length-1]
-        init.widgets.forEach(w => page.appendChild(w))
-    }, function (widgets) {return !!widgets}, 'Fit all widgets into page'),
-
-    defaultHandler: Handler( function (init){
-        return assemblePDF(init)
-    }, function (init) {return !!init.widgets},'Iterate over widgets and fit as many into page as posible')
+/////////////////      Strategy 2     /////////////////////
+var ScaleDownSingleWidget = function (){}
+ScaleDownSingleWidget.prototype = new Handler();
+ScaleDownSingleWidget.prototype.handleRequest = function (request){
+    if(request.itemHeight > request.pageHeight){
+        console.log("ScaleDownSingleWidget")
+        Commander.execute('appendWidget', request.pages, request.items[request.index]);
+        Commander.execute('scaleDownWidget', request.items[request.index], (constants.PAGE_HEIGHT / request.itemHeight))
+        Commander.execute('addFooter', request.pages, request.mode, request.print);
+        Commander.execute('finishPage');
+        return ;
+    }
+    this.next.handleRequest(request);
 }
 
-//Define each handler's next step
-handlers.fitAllWidgetsIntoPage.setNextStep(handlers.defaultHandler);
-handlers.defaultHandler.setNextStep(null);
+/////////////////      Strategy  3   /////////////////////
+var AddWidgetAndContinue = function (){}
+AddWidgetAndContinue.prototype = new Handler();
+AddWidgetAndContinue.prototype.handleRequest = function (request){
+    if(request.debt <= 0){
+        console.log("AddWidgetAndContinue")
+        Commander.execute('appendWidget', request.pages, request.items[request.index])
+        if (request.index+1 === request.items.length) {
+            Commander.execute('addFooter', request.pages, request.mode, request.print)
+        }
+        return ;
+    }
+    this.next.handleRequest(request);
+}
+
+/////////////////      Strategy   4   /////////////////////
+var ScaleDownWidgets = function (){}
+ScaleDownWidgets.prototype = new Handler();
+ScaleDownWidgets.prototype.handleRequest = function (request){
+    if(request.debt <= request.scaleDownThreshold){
+        console.log("ScaleDownWidgets")
+        Commander.execute('appendWidget', request.pages, request.items[request.index]);
+        Commander.execute('addFooter', request.pages, request.mode, request.print);
+        Commander.execute('scaleDownWidget', request.items[request.index-1]);
+        Commander.execute('scaleDownWidget', request.items[request.index]);
+        Commander.execute('finishPage');
+        return ;
+    }
+    this.next.handleRequest(request);
+}
+
+/////////////////      Strategy  5    /////////////////////
+var RemoveFooter = function (){}
+RemoveFooter.prototype = new Handler();
+RemoveFooter.prototype.handleRequest = function (request){
+    if(request.debt <= request.removeFooterThreshold){
+        console.log("RemoveFooter")
+        return;
+    }
+    this.next.handleRequest(request);
+}
+
+/////////////////      Strategy  6   /////////////////////
+var Default = function (){}
+Default.prototype = new Handler();
+Default.prototype.handleRequest = function (request){
+    console.log("DefaultAssignment")
+    Commander.execute('addFooter', request.pages, request.mode, request.print);
+    Commander.execute('finishPage')
+    return ;
+}
 
 
 
+//Chain of responsibility for widgets assignments
+var chain = {
+    handle: function (request) {
+        var strategy0 = new CheckIfPageFinished();
+        var strategy1 = new ScaleDownSingleWidget();
+        var strategy2 = new AddWidgetAndContinue();
+        var strategy3 = new ScaleDownWidgets();
+        var strategy4 = new RemoveFooter();
+        var strategy5 = new Default();
 
+        strategy0
+            .setNext(strategy1)
+            .setNext(strategy2)
+            .setNext(strategy3)
+            .setNext(strategy4)
+            .setNext(strategy5);
 
-
-
-
+        strategy0.handleRequest(request);
+    }
+}
---
+++ src/commander.js
@@ -0,0 +1,208 @@
+/*
+* Command dispatcher
+* */
+var Commander = {
+    state: {
+        isPageFinished: false,
+        sumOfHeights: 0
+    },
+    execute: function ( command ) {
+        /*
+        * Commands collection
+        * */
+        var Commands = {
+            assemblePDF: function(props) {
+                for (var i = 0; i < props.items.length; i++) {
+                    var itemHeight = props.items[i].offsetHeight;
+                    var debt = (Commander.state.sumOfHeights + itemHeight) - props.pageHeight ; //space to fill
+                    var request = {
+                        index: i,
+                        items: props.items,
+                        isPageFinished: Commander.state.isPageFinished,
+                        pageHeight: constants.PAGE_HEIGHT,
+                        pages: props.pages,
+                        print: props.print,
+                        mode: props.mode,
+                        itemHeight: itemHeight,
+                        debt: debt,
+                        removeFooterThreshold: constants.DEFAULT_SKIP_FOOTER_THRESHOLD,
+                        scaleDownThreshold: constants.DEFAULT_SCALE_DOWN
+                    }
+                    chain.handle(request)
+                }
+                if (props.mode === 'landscape'){
+                    Commander.execute('transformToLandscape', props.print, props.pages);
+                }
+
+            },
+            makeLandscapeFooter: function(props) {
+                var tuple = Commander.execute('makeFooterAndWrapper', {pageIndex: props.pagesIndex[0], mode: 'landscape'});
+                //left section
+                var leftSection = document.createElement('small');
+
+                leftSection.innerHTML = 'Page ' + props.pagesIndex[1];
+                if (!props.noLastPage){
+                    tuple[1].appendChild(leftSection);
+                }
+                return tuple[0]
+            },
+            nodeListToIterable: function(nodeList) {
+                var items = [];
+                for (var i = 0; i < nodeList.length; i++){
+                    items.push(nodeList[i])
+                }
+                return items;
+            },
+            formatScale: function(scale){
+                return 'scale('+scale+')'
+            },
+            resetPageStatus: function () {
+                Commander.state.isPageFinished = false;
+            },
+            scalePage: function (props) {
+
+                if (props.page){
+                    if (props.scale !== 0 && props.scale !== 1){
+                        props.page.style['transform'] = Commander.execute('formatScale', props.scale);
+                    }
+                }
+            },
+            createNewPage: function (props) {
+                var page = document.createElement("div");
+                var pageWrapper = document.createElement("div");
+                pageWrapper.setAttribute('class', 'pdf-page');
+                pageWrapper.appendChild(page);
+                props.print.appendChild(pageWrapper);
+                props.pages.push(page);
+        /*        page.style.border = '1px solid green';*/
+                return page;
+            },
+            createLandscapePage: function (props) {
+                var page1 = props.pages[0];
+                var page2 = props.pages[1];
+                var pageWrapper = document.createElement("div");
+                pageWrapper.style['display'] = 'flex';
+                pageWrapper.style['align-items'] = 'center';
+                pageWrapper.style['justify-content'] = 'center';
+                pageWrapper.setAttribute('class', 'pdf-page-landscape');
+                pageWrapper.appendChild(page1);
+                if (page2) {
+                    pageWrapper.appendChild(page2);
+                }
+                return pageWrapper;
+            },
+            scaleDownWidget: function (widget, customScale) {
+                var scale = constants.PAGE_HEIGHT/ Commander.state.sumOfHeights;
+                if (customScale){
+                    scale = customScale
+                }
+                widget.style['transform'] = Commander.execute('formatScale', scale)
+            },
+            finishPage: function () {
+                Commander.state.sumOfHeights = 0;
+                Commander.state.isPageFinished = true;
+            },
+            addFooter: function (pages, mode, print) {
+                var page = pages[pages.length -1];
+                var footerTuple = Commander.execute('makeFooterAndWrapper', {pageIndex: pages.length, mode: mode, width: page.offsetWidth});
+                footerTuple[0].style['width'] = page.offsetWidth + 'px';
+                print.appendChild(footerTuple[0]);
+            },
+            appendWidget: function (pages, widget) {
+                var page = pages[pages.length -1];
+                Commander.state.sumOfHeights += widget.offsetHeight;
+   /*             widget.style.border = '1px dashed red'*/
+                page.appendChild(widget);
+            },
+            makeSeparator: function(width) {
+                const separator = document.createElement('hr');
+                separator.style.width = width+'px'
+                separator.style['margin-left'] = Math.floor(width*0.01) + 'px';
+                separator.style['border-top'] = '2px solid grey'
+                return separator
+            },
+            makeFooterAndWrapper: function (props){
+                var footerWrapper = document.createElement('div');
+                var footer = document.createElement("div");
+                var rightSection = document.createElement('small');
+                var footerSignature = document.createElement('small');
+                var customWidth = props.mode === 'landscape'? props.width*2: props.width;
+
+                //footer
+                footer.style['padding'] = '10px'
+                footer.style['font-weight'] = 'bold';
+                footer.style['margin-top'] = '10px';
+                footer.style['margin-bottom'] = '10px';
+                footer.style['display'] = 'flex';
+                footer.style['justify-content'] = 'space-between';
+                footer.style['width'] = customWidth + 'px'
+                footer.setAttribute('class', 'pdf-footer');
+
+                //Signature
+                footerSignature.style['color'] = 'grey';
+                footerSignature.style['top'] = '10px';
+                footerSignature.style['left'] = '50%'
+                footerSignature.style['position'] = 'relative';
+                footerSignature.style['margin-left'] = 'auto';
+                footerSignature.style['margin-right'] = 'auto';
+                footerSignature.innerHTML = 'SEARS 2020';
+
+                if (props.mode === 'portrait'){
+                    rightSection.style['margin-right'] = '0';
+                    rightSection.style['margin-left'] = 'auto';
+                }
+
+                rightSection.innerHTML = 'page ' + props.pageIndex;
+                footer.appendChild(rightSection);
+                footerWrapper.style.width = customWidth + 'px'
+
+                footerWrapper.appendChild(Commander.execute('makeSeparator', customWidth));
+                footerWrapper.appendChild(footerSignature)
+                footerWrapper.appendChild(footer)
+                var returnValue = [footerWrapper, footer] //INTERESANTE, no puedo retornar {footerWrapper, footer}?? o que sucede
+                return returnValue
+            },
+            hideRemainingElements: function() {
+                // Hide extra element
+                document.querySelector("#main > div.mail__container").style.display = "none";
+            },
+            markDocAsReady: function() {
+                console.log('---------------------- COMPLETE------------------------');
+
+                if (window.pdfdone) {
+                    window.pdfdone();
+                }
+                if (true) {
+                    var readyElem = document.createElement("div");
+                    readyElem.setAttribute('id', 'pdf-ready');
+                    document.body.appendChild(readyElem);
+                }
+                window.status = 'ready';
+            },
+            transformToLandscape: function (print, pages) {
+                var pages_tuples = [];
+                for (var i=0; i < pages.length; i+=2) {
+                    var sumOfWidths = pages[i].offsetWidth;
+                    if ( pages[i+1]) {
+                        sumOfWidths += pages[i+1].offsetWidth
+                        Commander.execute("scalePage", {page: pages[i], scale: constants.PAGE_HEIGHT/sumOfWidths})
+                        Commander.execute("scalePage", {page: pages[i+1], scale: constants.PAGE_HEIGHT/sumOfWidths})
+                    }
+
+                    pages_tuples.push([pages[i], pages[i+1]]);
+                }
+                print.innerHTML = ''
+                for (var i=0; i< pages_tuples.length; i++){
+                    var landScape = Commander.execute("createLandscapePage", {pages: pages_tuples[i]});
+                    print.appendChild(landScape);
+                    print.appendChild(Commander.execute('makeLandscapeFooter', {pagesIndex: [i+i +1, i+i+2], noLastPage: !pages_tuples[i][1], mode: 'landscape'}))
+                }
+
+            }
+        }
+        if (!Commands[command]) {
+            throw 'ERROR: ' + command + ' <- command not found in commands collection.'
+        }
+        return Commands[command].apply( Commands, [].slice.call(arguments, 1) );
+    }
+}
